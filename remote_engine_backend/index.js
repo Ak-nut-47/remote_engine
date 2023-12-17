813963c24a1cffd21b7ed6abdf58e0ba752b9256
Animesh Kalita <104593018+Ak-nut-47@users.noreply.github.com>
2023-12-17 23:09:04 +0530

    Starting to create backend from scratch again, basic structure in progress

---
+++ remote_engine_backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const connection = require('./config/db');
+const signInRouter = require('./routes/signin.route');
+const signUpRouter = require('./routes/signup.route');
+const cors = require("cors");
+const skillRouter = require('./routes/skill.route');
+const app = express();
+
+
+dotenv.config();
+
+
+
+// Middleware
+app.use(express.json());
+app.use(cors())
+app.use("/developer/signin", signInRouter)
+app.use("/developer/signup", signUpRouter)
+app.use("/skills", skillRouter)
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, async () => {
+    await connection;
+    console.log(`Server is running on http://localhost:${PORT} And is connected to the Database`);
+});
